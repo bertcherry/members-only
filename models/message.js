f96febb745a22b2c954fd2d46e89d8a51b37f312
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-10-03 11:04:17 -0700

    add message model

---
+++ models/message.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
+
+const MessageSchema = new Schema({
+    title: { type: String, required: true, maxLength: 100 },
+    text: { type: String, required: true },
+    time: { type: Date, default: Date.now },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+MessageSchema.virtual('time_formatted').get(function() {
+    return DateTime.fromJSDate(this.time).toLocaleString(DateTime.DATETIME_FULL);
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
