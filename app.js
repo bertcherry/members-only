28374d8a4f6c81d66e131dbf5d1deea4ca0ec175
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-10-03 12:41:47 -0700

    add nav and head partials

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
